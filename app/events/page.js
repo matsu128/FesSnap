8aaed56a9c85db2a8a1496e4250c0157f5d5bb52
matsu128 <matsumoto1282002@icloud.com>
2025-07-14 19:39:41 +0900

    fix: /events SuspenseラップでuseSearchParamsエラー解消

--- app/events/page.js
+++ app/events/page.js
@@ -1,7 +1,12 @@
 "use client";
 // イベントリストページ
 import EventListMain from '../../components/organisms/EventListMain';
+import { Suspense } from 'react';
  
 export default function EventsPage() {
-  return <EventListMain />;
+  return (
+    <Suspense>
+      <EventListMain />
+    </Suspense>
+  );
 } 
