3ea8f4bd252ca6a70e70c0b52afc4fc166355b88
matsu128 <matsumoto1282002@icloud.com>
2025-07-14 22:42:41 +0900

    fix: SuspenseでAdminMainをラップしuseSearchParamsエラー解消

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,8 +1,11 @@
 "use client";
-// 主催者ページ
-// AdminMain organismを使って要件を満たす
+import { Suspense } from "react";
 import AdminMain from '../../components/organisms/AdminMain';
  
 export default function AdminPage() {
-  return <AdminMain />;
+  return (
+    <Suspense>
+      <AdminMain />
+    </Suspense>
+  );
 } 
